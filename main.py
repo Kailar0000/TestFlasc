959d352463c23649efc65920a81c8a4e07870f76
Kailar0000 <kailar0000@gmail.com>
2024-07-19 14:49:10 +0300

    "Session commit"

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from flask import Flask
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, session
 
 from config import token
 from api import get_weather
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/", methods=['GET', 'POST'])
 def start_page():
-    if request.method == 'POST':
-        city = request.form['city']
-        return redirect(f'/wether_page/{city}')
+    if 'city' in session:
+        return redirect(f'/wether_page/{session["city"]}')
+    else:
+        if request.method == 'POST':
+            city = request.form['city']
+            session['city'] = city
+            return redirect(f'/wether_page/{city}')
     return render_template('home.html')
 
 @app.route("/wether_page/<city>", methods=['GET', 'POST'])
